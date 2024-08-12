3eb118c6a05f019ed660ee5a1543d88124b63987
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-08-12 21:42:22 +0800

    update site for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://LeeSKII.github.io',
-  base: '/',
+  site: 'https://leeskii.github.io',
+  base: '/blog',
   integrations: [preact()]
 });
