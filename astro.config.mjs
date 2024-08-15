adef095c62f82a336f42f61fa69707688b5f9735
DESKTOP-TJ325BT\Administrator <leeskiikseel@outlook.com>
2024-08-15 10:44:36 +0800

    add link with markdown anchor and fix bug link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://leeskii.github.io',
   base: '/blog',
-  integrations: [preact()]
+  integrations: [preact()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: 'text', value: ' 🔗' }
+        }
+      ],
+    ]
+  },
 });
