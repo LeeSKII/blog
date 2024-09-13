157dd89281aa17def40a023bba712a3822c43256
Papilon <leeskiikseel@outlook.com>
2024-09-13 14:45:03 +0800

    update style

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,31 @@
-export interface IAnimateProps {
+type CssProperty = keyof Omit<
+  CSSStyleDeclaration,
+  | "item"
+  | "setProperty"
+  | "removeProperty"
+  | "getPropertyValue"
+  | "getPropertyPriority"
+  | "parentRule"
+  | "length"
+  | "cssFloat"
+  | "cssText"
+  | typeof Symbol.iterator
+  | number
+>;
+
+type CSSProperties = {
+  [key in CssProperty]?: string | number | undefined;
+};
+
+export interface IBaseProps {
+  id?: string;
+  name?: string;
+  style?: string | CSSProperties | null | undefined;
+}
+
+export interface IAnimateProps extends IBaseProps {
   animate?: "slide" | "fade" | "none" | "initial";
+}
+export interface IClassNameProps extends IBaseProps {
+  className?: string;
 }
