4ffaca5d36dd8ee887340eeb67fc60bf5248e6a0
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-12-22 15:27:47 +0800

    update

--- src/pages/pico/prime-ui/Examples.vue
+++ src/pages/pico/prime-ui/Examples.vue
@@ -4,11 +4,13 @@
   <DatePicker v-model="date" />
   {{ date }}
   {{ checked }}
+  <Drawer />
 </template>
 <script setup lang="ts">
 import Button from "primevue/button";
 import DatePicker from "primevue/datepicker";
 import Checkbox from "primevue/checkbox";
+import Drawer from "./Drawer.vue";
 import { ref } from "vue";
 const checked = ref(false);
 const date = ref(new Date());
